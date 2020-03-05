5447ee296798d167b96f058427ebae0683d07315
jasuade <jaraspg@gmail.com>
2020-03-05 15:56:13 +0100

    math exer

---
+++ src/math.go
@@ -0,0 +1,17 @@
+//Playing with math and imports
+
+package main
+
+//the imports could be included in braces and like that:
+//import "fmt"
+//import "math"
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	//NOTE the PrintF for the display of variables
+	fmt.Printf("The square root of 7 is %g, I think.\n", math.Sqrt(7))
+}
